b88fd57e868dbdb57a55c2ca84f1cb0ce8ce3359
Alif Rizki Pambudi <alifrizkipambudi@gmail.com>
2021-06-05 21:14:22 +0700

    Initial Commit

---
+++ TemperatureConversion/TemperatureConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversionApp.swift
+//  TemperatureConversion
+//
+//  Created by alip on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
